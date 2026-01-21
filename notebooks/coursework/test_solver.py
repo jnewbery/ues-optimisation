d6889747966de1cf80a104f1c57e3bb8c964b721
John Newbery <john@johnnewbery.com>
2026-01-21 08:24:34 +0000

    Add a test for the solver

---
+++ notebooks/coursework/test_solver.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+from layout import load_layout
+from model import build_and_solve_model
+
+layout_path = (
+    Path("data")
+    / "town_layout_small.json"
+)
+town = load_layout(layout_path)
+
+optimisation_result = build_and_solve_model(
+    town,
+    cost_energy_center=2_000_000.0,
+    cost_pipe=100_000.0,
+)
+
+breakpoint()
